d699505d8a390be291d4673e8778a754fe861ade
Jérémy Buget <jbuget@octo.com>
2013-11-19 01:06:52 +0100

    Add tooltip images

--- mathys/js/main.js
+++ mathys/js/main.js
@@ -1,11 +1,23 @@
-$(document).ready(function()
-{
+$(document).ready(function () {
+    $.getJSON("data/data.json", function (data) {
+        $.each(data,
+            function (index, image) {
+                $('#images').append('<li class="thumbnail" data-image="' + image.medium + '"><a href="' + image.big + '"><img alt="' + image.title + '" src="' + image.small + '"></a></li>');
+            }
+        );
 
-  $.getJSON("data/data.json", function(data) {
-      $.each( data, function( index, image) {
-        $('#images').append('<li class="thumbnail"><a href="' + image.big + '"><img alt="' + image.title + '" src="' + image.small + '"></img></a></li>');
-      });  
-  });
+        $(document).tooltip({
+            items: "[data-image]",
+            content: function () {
+                var element = $(this);
+                var image = element.data("image");
+                console.log("image = " + image);
+                var markup = '<img class="image" height="300" alt="' + image + '" src="' + image + '">';
+                return markup;
+            },
+            track: true
+        });
+
+    });
 });
-
 
