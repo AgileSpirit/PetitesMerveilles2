5b1a31138483e619b24b2b38ffeecdcc9afea5d5
Jérémy Buget <jbuget@octo.com>
2013-11-25 17:45:08 +0100

    Refactor

--- mathys/js/main.js
+++ mathys/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $.getJSON("data/data.json", function (data) {
         $.each(data,
             function (index, image) {
-                $('#images').append('<li class="thumbnail" data-image="' + image.medium + '"><a href="' + image.big + '"><img alt="' + image.title + '" src="' + image.small + '"></a></li>');
+                $('#images').append('<li class="thumbnail" data-image="' + image.medium + '"><a href="' + image.big + '"><img alt="' + image.title + '" src="' + image.small + '" onerror="this.onerror=null;this.src=\'img/noimage.png\'"></a></li>');
             }
         );
 
@@ -12,7 +12,7 @@ $(document).ready(function () {
                 var element = $(this);
                 var image = element.data("image");
                 console.log("image = " + image);
-                var markup = '<img class="image" onerror="this.src = \"img/default.png\"" height="300" alt="' + image + '" src="' + image + '">';
+                var markup = '<img class="image" height="300" alt="' + image + '" src="' + image + '">';
                 return markup;
             },
             track: true
